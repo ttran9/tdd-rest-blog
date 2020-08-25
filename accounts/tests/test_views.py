7b02f38b4790d19219124f8c8f4d2d52d1a89512
ttran9 <toddtran9@gmail.com>
2020-08-25 14:11:47 -0700

    commenting out unused class.

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -1,34 +1,34 @@
 from django.test import TestCase
 
 
-class DemoTest(TestCase):
-    """
-    setUp
-    setUpClass
-    setUpTestData
-    tearDown
-    tearDownClass
-    """
+# class DemoTest(TestCase):
+#     """
+#     setUp
+#     setUpClass
+#     setUpTestData
+#     tearDown
+#     tearDownClass
+#     """
 
-    def setUp(self):
-        """Loaded once per test."""
-        # if you have something you need reset for each test case this will be useful.
-        pass
+#     def setUp(self):
+#         """Loaded once per test."""
+#         # if you have something you need reset for each test case this will be useful.
+#         pass
 
-    def setUpClass(cls):
-        """Once per TestCase class"""
-        pass
+#     def setUpClass(cls):
+#         """Once per TestCase class"""
+#         pass
 
-    def setUpTestData(cls):
-        """Once per TestCase class"""
-        pass
+#     def setUpTestData(cls):
+#         """Once per TestCase class"""
+#         pass
 
-    def test_some_func(self):
-        pass
+#     def test_some_func(self):
+#         pass
 
-    def tearDown(self):
-        pass
+#     def tearDown(self):
+#         pass
 
-    def tearDownClass(cls):
-        """Once per TestCase class"""
-        pass
+#     def tearDownClass(cls):
+#         """Once per TestCase class"""
+#         pass
