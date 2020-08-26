c99da61c2f74cb103d99585a89b779d15012d93d
ttran9 <toddtran9@gmail.com>
2020-08-25 18:09:10 -0700

    adding test.

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -1,7 +1,11 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
 from django.shortcuts import reverse
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AnonymousUser
 from accounts.tests.common.mixins import ColumnTestDataMixin
+from accounts import views
+
+User = get_user_model()
 
 
 class ColumnListViewTest(TestCase):
@@ -16,6 +20,39 @@ class ColumnListViewTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Test column")
         self.assertContains(response, "Column list")
+
+
+class ColumnFeedViewTest(TestCase):
+    fixtures = [
+        'users.json',
+        'columns.json',
+        'subscriptions.json'
+    ]
+
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.reader = User.objects.get(email="reader@jd.com")
+
+    def test_get_column_feed(self):
+        # test the feed list for a specific user.
+        # factory allows us to create a request manually.
+        request = self.factory.get(reverse("columns:feed"))
+        request.user = self.reader  # assign user
+        # request.user = AnonymousUser()
+        # import view that handles the request.
+        # take view itself and test the way the view is executed.
+        # pass the request into the view.
+        # passed as a function then we pass request into the function.
+        response = views.ColumnFeedView.as_view()(request)
+        self.assertEqual(response.status_code, 200)
+        # convert from queryset to list.
+        column_names = list(request.user.user_subscriptions.all().values_list(
+            'column__name', flat=True
+        ))
+        print(column_names)
+
+        for name in column_names:
+            self.assertContains(response, name)
 
 
 # class DemoTest(TestCase):
